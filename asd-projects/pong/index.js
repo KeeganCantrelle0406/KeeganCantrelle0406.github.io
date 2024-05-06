4fbd93226e72a709bb608fa5a0ef58ea8e76ce12
KeeganCantrelle0406 <Cantre356@stcharles.k12.la.us>
2024-05-06 15:43:41 +0000

    message saying what you are pushing

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -12,7 +12,21 @@ function runProgram(){
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
-
+  var PAD1 = factory("#paddle1", 50, 50, 0, 0, 20, 80);
+  var PAD2 = factory("#paddle2", 150, 150, 0, 0, 20, 80);
+  var BALL = factory("#ball", 100, 100, 0, 0, 20, 20);
+  
+  function factory  (id, x, y, speedX, speedY, width, height){
+      var gameItem = {}
+      gameItem.id = id
+      gameItem.x = x
+      gameItem.y = y
+      gameItem.speedX = speedX
+      gameItem.speedy = speedY
+      gameItem.width = width
+      gameItem.height = height
+      return gameItem
+  }
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
